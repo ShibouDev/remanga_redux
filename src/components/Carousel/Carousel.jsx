72bce8445f45253e42420838aab7a113ef88320c
Shibou <99544992+ShibouDev@users.noreply.github.com>
2022-09-02 10:32:42 +0500

    add slick and title

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
+import { useSelector, useDispatch } from 'react-redux';
 import { darkTheme, lightTheme } from '../theme/theme';
+import {FormGroup, Paper, IconButton, Stack, Container, Avatar, TextField, Menu, MenuItem,ListItemIcon,ListItemText } from "@mui/material"
 import {isDesktop} from "../Core/Breakpoints/Breakpoints"
+import titles from '../Data/titles';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
-import "~slick-carousel/slick/slick.css"; 
-import "~slick-carousel/slick/slick-theme.css";
 export const Carousel = () => {
 const [w, setW] = useState(window.innerWidth)
 useEffect(() => {
@@ -18,12 +21,24 @@ const settingSlider = {
     infinity: false,
     speed: 500,
     slidesToShow: w > isDesktop ? 8 : 3,
-    slidesToScrill: w > isDesktop ? 4 : 3
+    slidesToScrill: w > isDesktop ? 4 : 2,
+    infinity: false,
 }
     return(
       <ThemeProvider theme={theme.darkTheme ? darkTheme : lightTheme}>
+        <Paper sx={{
+            maxWidth: 'xl',
+            borderRadius: '0', 
+            boxShadow: "none",
+        }}>
         <Slider {...settingSlider}>
+            {titles.map((el) => 
+            <Container>
+                <img src={require('../../assets/images/' + el.imgname + '.jpg')}></img>
+                <p>{el.name}</p>
+            </Container>)}
         </Slider>
+        </Paper>
       </ThemeProvider>
     )
 }
---
+++ src/components/Data/titles.js
@@ -0,0 +1,25 @@
+import title_one from "../../assets/images/title_one.jpg"
+import title_two from "../../assets/images/title_two.jpg"
+export const titles = [
+    {
+        name: "Реинкарнация мудреца из другого мира",
+        img: "../../assets/images/title_one.jpg",
+        imgname: "title_one",
+    },
+    {
+        name: "Нуб максимального уровня",
+        img: "../../assets/images/title_two.jpg",
+        imgname: "title_two",
+    },
+    {
+        name: "Нуб максимального уровня",
+        img: "../../assets/images/title_two.jpg",
+        imgname: "title_two",
+    },
+    {
+        name: "Нуб максимального уровня",
+        img: "../../assets/images/title_two.jpg",
+        imgname: "title_two",
+    }
+]
+export default titles
